c138cb273ebbd5bb19aac889d70662af6b4ab0db
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-04-22 19:01:44 -0700

    Added xcffib dependency

--- setup.py
+++ setup.py
@@ -1,29 +1,23 @@
 from __future__ import print_function
+from io import open
 import sys
 
-from distutils import sysconfig
-from distutils.core import setup
-
-try:
-    from xpybutil.compat import xproto, xinerama, randr
-except:
-    print ('')
-    print ('xpybutil requires the X Python Binding')
-    print ('See: http://cgit.freedesktop.org/xcb/xpyb/')
-    print ('More options: xpyb-ng:', 'https://github.com/dequis/xpyb-ng',
-           'and xcffib:', 'https://github.com/tych0/xcffib')
-    sys.exit(1)
+from setuptools import (
+    find_packages,
+    setup,
+)
 
 setup(
-    name = "xpybutil",
-    author = "Andrew Gallant",
-    author_email = "andrew@pytyle.com",
-    version = "0.0.1",
-    license = "WTFPL",
-    description = "An incomplete xcb-util port plus some extras",
-    long_description = "See README",
-    url = "http://pytyle.com",
-    platforms = 'POSIX',
-    packages = ['xpybutil'],
-    data_files = [('share/doc/xpybutil', ['README', 'COPYING'])]
+    name="xpybutil",
+    maintainer="Fenner Macrae",
+    maintainer_email="fmacrae.dev@gmail.com",
+    version="0.0.2",
+    license="WTFPL",
+    description="An incomplete xcb-util port plus some extras",
+    install_requires=['xcffib']
+    long_description=open('README', 'r', encoding='utf-8').read(),
+    url="http://github.com/fennerm/xpybutil",
+    packages=find_packages(),
+    keywords='xorg X xcb xpyb xcffib',
+    data_files=[('share/doc/xpybutil', ['README', 'LICENSE'])]
 )
